25f75227b04c55254188fc3dc50339979c019936
brightsheden <ibrahimsherifdeen2020@gmail.com>
2024-09-23 09:50:52 -1200

    updated cors

--- backend/settings.py
+++ backend/settings.py
@@ -68,6 +68,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:4173'
+    'http://localhost',
+    'capacitor://localhost',
 ]
 
 
@@ -136,8 +138,8 @@ DATABASES = {
    }
 }
 
-"""
-DATABASES = {
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
