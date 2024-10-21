cf1b9bc1dc895aa59b62a647bdf398f428d1a057
brightsheden <ibrahimsherifdeen2020@gmail.com>
2024-10-21 01:27:20 -1100

    add updated_at to the model

--- base/models.py
+++ base/models.py
@@ -57,6 +57,7 @@ class Equipment(models.Model):
     remarks = models.TextField(blank=True, null=True)
     state = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
